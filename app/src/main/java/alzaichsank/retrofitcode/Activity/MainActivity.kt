340c1fbd7807768ecd096c6c2d6ccefd91e46dbd
Alza Devloper <alzaichsank@gmail.com>
2018-08-13 21:47:12 +0700

    AK: import library retrofit

---
+++ app/src/main/java/alzaichsank/retrofitcode/Activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package alzaichsank.retrofitcode.Activity
+
+import alzaichsank.retrofitcode.R
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
